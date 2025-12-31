1001b3069e700774df1bf29c6f3dac7539de2825
Donald MXolisi <donald.mxolisi@proton.me>
2025-12-31 06:43:59 -0800

    update

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://ec2-44-210-134-149.compute-1.amazonaws.com", {
+export const socket = io("https://api.blackfroglabs.co.za", {
   autoConnect: false,
 });
