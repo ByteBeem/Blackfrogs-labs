678d01f49effc3a9c437448a4ece37995702d429
Donald MXolisi <donald.mxolisi@proton.me>
2025-12-31 04:58:30 -0800

    update

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+export const socket = io("http://ec2-44-210-134-149.compute-1.amazonaws.com", {
   autoConnect: false,
 });
